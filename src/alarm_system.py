4816a2456ddefba61b6fb1afab4c18d97418c658
Denise Ratasich <denise.ratasich@rpia.com>
2015-04-01 14:12:14 +0200

    fix camera (record)

--- src/alarm_system.py
+++ src/alarm_system.py
@@ -3,6 +3,9 @@
 # @file
 # @author Denise Ratasich
 # @date 22.03.2015
+#
+# @brief Captures images and records videos when the motion sensor
+# triggers.
 ##
 
 import logging
@@ -14,8 +17,21 @@ from camera import CameraController
 PIN_MOTION_SENSOR = 15  # pin from the motion sensor near the camera (directly)
 PIN_ALARM = 16          # additional pin from the alarm system switch
 
+# will be initialized (main)
+motion_sensor = None
+cam = None
+
+##
+# @brief Called when the motion sensor triggers.
+##
 def callback_motion_sensor():
-    print 'Motion sensor: detection!'
+    logging.info('Motion sensor: detection!')
+#    cam.capture()
+    cam.record()
+
+##
+# @brief Start of the program.
+##
 
 # set logging settings
 root = logging.getLogger()
@@ -28,17 +44,18 @@ root.addHandler(ch)
 
 # main
 try:
-    # test motion sensor
+    # init motion sensor
     motion_sensor = gpio.EdgeMonitor(PIN_MOTION_SENSOR, gpio.FALLING, callback_motion_sensor)
     motion_sensor.start()
     logging.info('Make sure the motion sensor is connected to GPIO pin ' + 
 		 str(PIN_MOTION_SENSOR) + '.')
 
-    #cam = CameraController()
-    #cam.capture()
+    # init camera
+    cam = CameraController()
 
+    # wait recording
     while True:
-        time.sleep(3600)        # sleep for an hour or so
+        cam.work()
 
 except RuntimeWarning as e:
     logging.warn(e)
--- src/camera.py
+++ src/camera.py
@@ -9,11 +9,12 @@
 ##
 
 import picamera
+import io
 import logging
 import time
 
 RESOLUTION = (1280, 720)
-VIDEO_LENGTH = 20
+VIDEO_LENGTH = 5
 IMG_PREFIX = "picam_"
 IMG_SUFFIX = ""
 
@@ -28,15 +29,17 @@ class CameraController:
     def __init__(self):
         self._camera = picamera.PiCamera()
         self._camera.resolution = RESOLUTION
-        self._stream_circular = picamera.PiCameraCircularIO(self._camera, VIDEO_LENGTH)
+        self._stream_circular = picamera.PiCameraCircularIO(self._camera, seconds=VIDEO_LENGTH)
         logging.debug('CameraController: ' + str(self.__dict__))
         
         # make ready
         self._camera.start_preview()
         time.sleep(2)
+        self._camera.start_recording(self._stream_circular, format='h264')
         logging.debug('CameraController: started.')
 
     def __exit__(self):
+        self._camera.stop_recording()
         self._camera.close()
 	logging.debug('CameraController: exit.')
 
@@ -46,19 +49,27 @@ class CameraController:
     def capture(self):
         filename = IMG_PREFIX + time.strftime('%y-%m-%d_%H-%M-%S', time.localtime()) + IMG_SUFFIX + '.jpg'
         self._camera.capture(filename)
+        logging.debug('CameraController: captured image.')
 
     ##
     # @brief Record
     ##
-    def record():
+    def record(self):
         logging.debug('Writing video.')
         stream = self._stream_circular
         with stream.lock:
             # find the first header frame in the video
             for frame in stream.frames:
-                if frame.frame_type == self._camera.PiVideoFrameType.sps_header:
+                if frame.frame_type == picamera.PiVideoFrameType.sps_header:
                     stream.seek(frame.position)
                     break
             # write the rest of the stream to disk
             with io.open('motion.h264', 'wb') as output:
                 output.write(stream.read())
+        logging.debug('CameraController: recorded video of %ds.' % VIDEO_LENGTH)
+
+    ##
+    # @brief Sleep, but check continuously on recording errors.
+    ##
+    def work(self):
+        self._camera.wait_recording(1)
